a2217940403c6fccbec003d3641742b05af73871
Nathan-Simonnet <nathan.simonnet@gmail.com>
2024-11-02 11:47:53 +0100

    Initial commit. Ready to JS

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+// fetch('https://randomuser.me/api/?nat=fr&results=50')
+// .then((response) => response.json())
+// .then((data) => {console.log(data.results[0])})
+// .catch((error) => console.log(error))
